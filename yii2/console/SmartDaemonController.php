f2af64011a59a2a46a9b02c11b4a7e775e748c1a
jiangsyz <305460883@qq.com>
2018-06-07 11:03:19 +0800

    修改注释

--- yii2/console/SmartDaemonController.php
+++ yii2/console/SmartDaemonController.php
@@ -4,14 +4,14 @@ use Yii;
 class SmartDaemonController extends Controller{
 	private $beginLog=false;
 	//========================================
-	//命令行开始运行
+	//守护进程开始运行
 	public function begin(){
 		if(!$this->beginLog) $this->beginLog=Yii::$app->smartLog->consoleLog('begin');
 	}
 	//========================================
-	//命令行存活
+	//守护进程存活
 	public function alive(){Yii::$app->smartLog->consoleLog('alive');}
 	//========================================
-	//命令行休息
+	//守护进程休息
 	public function sleep(){sleep(Yii::$app->params["consoleSleep"]);}
 }
